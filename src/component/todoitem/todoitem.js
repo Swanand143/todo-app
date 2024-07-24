7a7bc64fd2e7b46175d407757ceac10f6f75138b
Swanand@143 <swanandgholap375@gmail.com>
2024-07-24 23:10:22 +0530

    todo item component added

--- src/component/todoitem/todoitem.js
+++ src/component/todoitem/todoitem.js
@@ -1,9 +1,18 @@
-import React from 'react'
-
-function todoitem() {
+import React from "react";
+import "./todoitem.css";
+function TodoItem({ task, category, onDelete }) {
   return (
-    <div>todoitem</div>
-  )
+    <div className="todo-item">
+      <span className="task">{task}</span>
+      <span className="category">{category}</span>
+      <img
+        src={"https://cdn-icons-png.flaticon.com/512/1828/1828843.png"}
+        alt="delete-btn"
+        className="delete-btn"
+        onClick={onDelete}
+      />
+    </div>
+  );
 }
 
-export default todoitem
+export default TodoItem;
